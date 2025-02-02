5d6670e58b07460183aeab969d20f55ca1c08b09
Andrew Whitman <andrew24.whitman@gmail.com>
2025-02-02 15:24:17 -0600

    updating model for consistent coloring and shadows from all angles

--- src/components/BibleModel.jsx
+++ src/components/BibleModel.jsx
@@ -1,107 +1,134 @@
 import React from 'react';
 import { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage } from '@react-three/drei';
+import { OrbitControls, Stage, AccumulativeShadows, RandomizedLight, Environment } from '@react-three/drei';
 import { Box } from '@react-three/drei';
 
 const BibleModel = ({ colors }) => {
   const bibleRef = useRef();
 
   return (
-    <Canvas shadows={false} dpr={[1, 2]} camera={{ position: [4, 2, 4], fov: 50 }} style={{ background: '#f8f8f8', width: '100%', height: '100%' }}>
-      <Stage environment="city" intensity={0.8} preset="rembrandt" shadows={false}>
-        <group ref={bibleRef}>
-          {/* Book block (pages) */}
-          <Box args={[2.2, 3.2, 0.8]} position={[0, 0, 0]}>
-            <meshStandardMaterial 
-              color={colors['guilding']} 
-              roughness={0.2} 
-              metalness={0.8}
-            />
-          </Box>
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [4, 2, 4], fov: 50 }} style={{ background: '#f8f8f8', width: '100%', height: '100%' }}>
+      <Environment preset="city" />
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={1}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <group ref={bibleRef}>
+        {/* Book block (pages) */}
+        <Box args={[2.2, 3.2, 0.8]} position={[0, 0, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['guilding']} 
+            roughness={0.2} 
+            metalness={0.8}
+            clearcoat={0.5}
+            clearcoatRoughness={0.3}
+          />
+        </Box>
 
-          {/* Leather cover outer - spine panel */}
-          <Box args={[0.1, 3.6, 0.8]} position={[-1.15, 0, 0]}>
-            <meshStandardMaterial 
-              color={colors['outer-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover outer - spine panel */}
+        <Box args={[0.1, 3.6, 0.8]} position={[-1.15, 0, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['outer-leather']} 
+            roughness={0.7} 
+            metalness={0.1}
+            clearcoat={0.1}
+            clearcoatRoughness={0.8}
+          />
+        </Box>
 
-          {/* Leather cover inner - spine panel */}
-          <Box args={[0.025, 3.5, 0.8]} position={[-1.1, 0, 0]}>
-            <meshStandardMaterial 
-              color={colors['inner-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover inner - spine panel */}
+        <Box args={[0.025, 3.5, 0.8]} position={[-1.1, 0, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['inner-leather']} 
+            roughness={0.6} 
+            metalness={0.1}
+            clearcoat={0.2}
+            clearcoatRoughness={0.6}
+          />
+        </Box>
 
-          {/* Leather cover outer - front panel */}
-          <Box args={[2.6, 3.6, 0.1]} position={[0.1, 0, 0.45]}>
-            <meshStandardMaterial 
-              color={colors['outer-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover outer - front panel */}
+        <Box args={[2.6, 3.6, 0.1]} position={[0.1, 0, 0.45]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['outer-leather']} 
+            roughness={0.7} 
+            metalness={0.1}
+            clearcoat={0.1}
+            clearcoatRoughness={0.8}
+          />
+        </Box>
 
-          {/* Leather cover inner - front panel */}
-          <Box args={[2.5, 3.5, 0.025]} position={[0.1, 0, 0.4]}>
-            <meshStandardMaterial 
-              color={colors['inner-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover inner - front panel */}
+        <Box args={[2.5, 3.5, 0.025]} position={[0.1, 0, 0.4]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['inner-leather']} 
+            roughness={0.6} 
+            metalness={0.1}
+            clearcoat={0.2}
+            clearcoatRoughness={0.6}
+          />
+        </Box>
 
-          {/* Leather cover outer - back panel */}
-          <Box args={[2.6, 3.6, 0.1]} position={[0.1, 0, -0.45]}>
-            <meshStandardMaterial 
-              color={colors['outer-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover outer - back panel */}
+        <Box args={[2.6, 3.6, 0.1]} position={[0.1, 0, -0.45]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['outer-leather']} 
+            roughness={0.7} 
+            metalness={0.1}
+            clearcoat={0.1}
+            clearcoatRoughness={0.8}
+          />
+        </Box>
 
-          {/* Leather cover inner - back panel */}
-          <Box args={[2.5, 3.5, 0.025]} position={[0.1, 0, -0.4]}>
-            <meshStandardMaterial 
-              color={colors['inner-leather']} 
-              roughness={0.9} 
-              metalness={0.1}
-            />
-          </Box>
+        {/* Leather cover inner - back panel */}
+        <Box args={[2.5, 3.5, 0.025]} position={[0.1, 0, -0.4]} castShadow receiveShadow>
+          <meshPhysicalMaterial 
+            color={colors['inner-leather']} 
+            roughness={0.6} 
+            metalness={0.1}
+            clearcoat={0.2}
+            clearcoatRoughness={0.6}
+          />
+        </Box>
 
-          {/* Ribbon 1 */}
-          <Box args={[0.3, 2, 0.01]} position={[-0.4, -2.1, 0]}>
-            <meshStandardMaterial
-              color={colors['ribbon-1']}
-              roughness={0.7}
-              metalness={0.1}
-            />
-          </Box>
+        {/* Ribbon 1 */}
+        <Box args={[0.3, 2, 0.01]} position={[-0.4, -2.1, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial
+            color={colors['ribbon-1']}
+            roughness={0.5}
+            metalness={0.1}
+            clearcoat={0.3}
+            clearcoatRoughness={0.4}
+          />
+        </Box>
 
-          {/* Ribbon 2 */}
-          <Box args={[0.3, 2, 0.01]} position={[0, -2.1, 0]}>
-            <meshStandardMaterial
-              color={colors['ribbon-2']}
-              roughness={0.7}
-              metalness={0.1}
-            />
-          </Box>
+        {/* Ribbon 2 */}
+        <Box args={[0.3, 2, 0.01]} position={[0, -2.1, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial
+            color={colors['ribbon-2']}
+            roughness={0.5}
+            metalness={0.1}
+            clearcoat={0.3}
+            clearcoatRoughness={0.4}
+          />
+        </Box>
 
-          {/* Ribbon 3 */}
-          <Box args={[0.3, 2, 0.01]} position={[0.4, -2.1, 0]}>
-            <meshStandardMaterial
-              color={colors['ribbon-3']}
-              roughness={0.7}
-              metalness={0.1}
-            />
-          </Box>
-        </group>
-      </Stage>
+        {/* Ribbon 3 */}
+        <Box args={[0.3, 2, 0.01]} position={[0.4, -2.1, 0]} castShadow receiveShadow>
+          <meshPhysicalMaterial
+            color={colors['ribbon-3']}
+            roughness={0.5}
+            metalness={0.1}
+            clearcoat={0.3}
+            clearcoatRoughness={0.4}
+          />
+        </Box>
+      </group>
       <OrbitControls
         enablePan={true}
         enableZoom={true}
